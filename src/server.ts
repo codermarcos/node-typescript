f2665abb26d24bea3535b545a94cd293d99bef32
codermarcos <coder.marcos@gmail.com>
2019-05-10 18:21:07 -0300

    :rocket: ts-node

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { AddressInfo } from 'net';
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+app
+  .get('/',
+    (_, res) => {
+      res.send('Hello ts-node!');
+    }
+  );
+
+
+server
+  .listen(3000, 'localhost',
+    () => {
+      const address = <AddressInfo>server.address();
+      console.log(`server starts at http://${address.address}:${address.port}`)
+    },
+  )
+
+export default server;
